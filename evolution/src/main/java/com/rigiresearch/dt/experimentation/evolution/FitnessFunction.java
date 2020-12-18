47e38abea91cdf8431b73547031fd403ab8ae19e
Miguel Jiménez <1284036+jachinte@users.noreply.github.com>
2020-12-17 21:34:17 -0500

    Turn static methods into instance methods

--- evolution/src/main/java/com/rigiresearch/dt/experimentation/evolution/FitnessFunction.java
+++ evolution/src/main/java/com/rigiresearch/dt/experimentation/evolution/FitnessFunction.java
@@ -1,12 +1,20 @@
 package com.rigiresearch.dt.experimentation.evolution;
 
+import lombok.RequiredArgsConstructor;
+
 /**
- * Defines the fitness function to use in the genetic algorithm.
+ * Defines the fitness function to reward minimizing a value.
  * @author Miguel Jimenez (miguel@uvic.ca)
  * @version $Id$
  * @since 0.1.0
  */
+@RequiredArgsConstructor
 public final class FitnessFunction {
+
+    /**
+     * The upper bound in the x axis.
+     */
+    private final double b;
 
     /**
      * Evaluates the function -1/a(x-a)^3, where 0<=x<=b and a=b/2.
@@ -14,26 +22,27 @@ public final class FitnessFunction {
      * f(x) is a positive number and is greater when x tends to 0, and is a
      * negative number and smaller when x tends to b.
      *
-     * @param b The upper bound in the x axis
+     * <p>Visit https://www.wolframalpha.com/input/?i=plot+-1%2F10%28x-10%29%5E3+from+0+to+20
+     * to see a plot of this function.</p>
+     *
      * @param x The value on the x axis to evaluate the function
      * @return A negative or positive number, or 0 when x = a
      */
-    public static double evaluate(final double b, final double x) {
-        final double a = b/2.0;
+    public double evaluate(final double x) {
+        final double a = this.b/2.0;
         final double y = -(1.0/a)* StrictMath.pow(x-a, 3.0);
         return y;
     }
 
     /**
-     * Same as {@link #evaluate(double, double)} but normalized.
-     * @param b The upper bound in the x axis
+     * Same as {@link #evaluate(double)} but normalized.
      * @param x The value on the x axis to evaluate the function
      * @return A number between 0 and 1
      */
-    public static double evaluateNormalized(final double b, final double x) {
-        final double y = FitnessFunction.evaluate(b, x);
-        final double min = FitnessFunction.evaluate(b, b);
-        final double max = FitnessFunction.evaluate(b, 0.0);
+    public double evaluateNormalized(final double x) {
+        final double y = this.evaluate(x);
+        final double min = this.evaluate(this.b);
+        final double max = this.evaluate(0.0);
         // Switch min and max so that when x=0, y=1 and when x=b, y=0
         return FitnessFunction.normalize(y, max, min);
     }
--- evolution/src/test/java/com/rigiresearch/dt/experimentation/evolution/FitnessFunctionTest.java
+++ evolution/src/test/java/com/rigiresearch/dt/experimentation/evolution/FitnessFunctionTest.java
@@ -18,25 +18,25 @@ class FitnessFunctionTest {
 
     @Test
     void testNonNormalized() {
-        final double b = 20.0;
+        final FitnessFunction function = new FitnessFunction(20.0);
         Assertions.assertTrue(
-            FitnessFunction.evaluate(b, 10.0) - 0.0 < FitnessFunctionTest.EPSILON,
+            function.evaluate(10.0) - 0.0 < FitnessFunctionTest.EPSILON,
             "Should be technically 0"
         );
         Assertions.assertTrue(
-            FitnessFunction.evaluate(b, 0.0) > 0.0,
+            function.evaluate(0.0) > 0.0,
             "Should be greater than 0"
         );
         Assertions.assertTrue(
-            FitnessFunction.evaluate(b, 20.0) < 0.0,
+            function.evaluate(20.0) < 0.0,
             "Should be less than 0"
         );
         Assertions.assertTrue(
-            FitnessFunction.evaluate(b, 5.0) > FitnessFunction.evaluate(b, 6.0),
+            function.evaluate(5.0) > function.evaluate(6.0),
             "Should increase when x tends to 0"
         );
         Assertions.assertTrue(
-            FitnessFunction.evaluate(b, 15.0) > FitnessFunction.evaluate(b, 16.0),
+            function.evaluate(15.0) > function.evaluate(16.0),
             "Should decrease when x tends to b"
         );
     }
@@ -44,18 +44,19 @@ class FitnessFunctionTest {
     @Test
     void testNormalized() {
         final double b = 10.0;
+        final FitnessFunction function = new FitnessFunction(b);
         Assertions.assertTrue(
-            FitnessFunction.evaluateNormalized(b, b/2.0) - 0.5 <
+            function.evaluateNormalized(b/2.0) - 0.5 <
                 FitnessFunctionTest.EPSILON,
             "Should be technically 0.5"
         );
         Assertions.assertTrue(
-            FitnessFunction.evaluateNormalized(b, b) - 0.0 <
+            function.evaluateNormalized(b) - 0.0 <
                 FitnessFunctionTest.EPSILON,
             "Should be technically 0"
         );
         Assertions.assertTrue(
-            FitnessFunction.evaluateNormalized(b, 0.0) - 1.0 <
+            function.evaluateNormalized(0.0) - 1.0 <
                 FitnessFunctionTest.EPSILON,
             "Should be technically 1"
         );
